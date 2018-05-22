2c81f4d993ef8326477003ac5aa17fddb158e8fa
Gua <332093548@qq.com>
2018-05-22 15:15:55 +0800

    采用两种方式可以实现数据赋值

--- src/cn/dw/model/Product.java
+++ src/cn/dw/model/Product.java
@@ -1,21 +1,59 @@
 package cn.dw.model;
 
+import java.util.Date;
 
-//此类事model 类->表，对象->记录，属性->字段
+// 此类是model  类->表    对象->记录  属性->字段  
 public class Product {
+
 	public Product() { // 对象创建此方法会被调用
 		System.out.println("Product.....");
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Product [productDaoImpl=" + productDaoImpl + ", name=" + name + ", price=" + price + ", date=" + date
+				+ "]";
+	}
+
+
+	public Product(String name, Double price) {
+		super();
+		System.out.println("Product(String name, Double price).......");
+		this.name = name;
+		this.price = price;
+	}
+
+	private ProductDaoImpl productDaoImpl;
+	
+	public ProductDaoImpl getProductDaoImpl() {
+		return productDaoImpl;
+	}
+
+	public void setProductDaoImpl(ProductDaoImpl productDaoImpl) {
+		this.productDaoImpl = productDaoImpl;
 	}
 
 	private String name;
 
 	private Double price;
+	
+	private Date date;
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
+		System.out.println("setName........");
 		this.name = name;
 	}
 
@@ -26,4 +64,5 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+
 }
